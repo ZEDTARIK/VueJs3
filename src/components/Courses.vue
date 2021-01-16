0d9887a494e7f8e255b43dc9925123e377c8c360
ettarak <ettarak.zouhair@gmail.com>
2021-01-16 15:21:19 +0100

    Coding, emit methode from child to Parent

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -11,46 +11,53 @@
    <div class="row">
     <div class="col-md-4"  v-for="course in Courses" :key="course.id">
         
-        <DisplayCours :title="course.title" :image="course.image" />
+        <DisplayCours :id="course.id" :title="course.title" :image="course.image" 
+                     @emitDeleteCourse="deleteOneCourse($event)" />
         
     </div>
    </div>
 
 </template>
 <script>
-
-import DisplayCours from './DisplayCours'
+import DisplayCours from "./DisplayCours";
 
 export default {
-
-    components: {
-        DisplayCours
+  components: {
+    DisplayCours,
+  },
+  data() {
+    return {
+      Courses: [
+        {
+          id: 1,
+          title: "Learn Angular",
+          image:
+            "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/nrOHB2iQTIiGe7hHX9O0",
+        },
+        {
+          id: 2,
+          title: "Learn NodeJs",
+          image:
+            "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/yMO0mZjvTm2pwyeOQUpT",
+        },
+        {
+          id: 3,
+          title: "Learn ReactJs",
+          image:
+            "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/C2oT8I1eTXGg69ytJ69f",
+        },
+      ],
+    };
+  },
+  methods: {
+    deleteOneCourse(id) {
+      this.Courses = this.Courses.filter((course) => course.id != id);
     },
-    data() {
-        return {
-            Courses : [
-                {
-                    id:1,
-                    title:'Learn Angular',
-                    image:'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/nrOHB2iQTIiGe7hHX9O0'
-                },
-                {
-                    id:2,
-                    title:'Learn NodeJs',
-                    image:'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/yMO0mZjvTm2pwyeOQUpT'
-                },
-                {
-                    id:3,
-                    title: 'Learn ReactJs',
-                    image:'https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/C2oT8I1eTXGg69ytJ69f'
-                }
-            ]
-        }
-    }
-}
+  },
+};
 </script>
 <style scoped>
-    h1 {
-        color: brown;
-    }
+h1 {
+  color: brown;
+}
 </style>
--- src/components/DisplayCours.vue
+++ src/components/DisplayCours.vue
@@ -6,14 +6,19 @@
                 <p class="card-text">
                     Lorem ipsum dolor sit amet consectetur.
                 </p>
+                <button class="btn btn-danger" @click="onDelete(id)">Delete</button>
             </div>
         </div>
 </template>
 <script>
 export default {
-    props : ['title', 'image']
-}
+  props: ["title", "image", "id"],
+  methods: {
+    onDelete(id) {
+      this.$emit("emitDeleteCourse", id);
+    },
+  },
+};
 </script>
 <style lang="">
-    
 </style>
